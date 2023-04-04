613fe04d7fd2a12acd5e6f3b93ed61bbf5d704a0
Amado Melguizo <56071729+Amado-Melguizo@users.noreply.github.com>
2023-04-04 21:13:14 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/sobrepatitas/'
-    : '/'
+  publicPath: process.env.NODE_ENV === 'production' ?
+  '/sobrepatitas/' : '/'
 }
